aa78b89b6287fdb8a93af2d71c5b7f92fd1e734d
kim3z <kim.lehtinen@hotmail.com>
2020-07-08 18:43:28 +0300

    Clean system after deployment

--- remove_unused_images.sh
+++ remove_unused_images.sh
@@ -1,1 +1,8 @@
-ssh deploy@164.90.168.125 "docker images -q | xargs docker rmi"
+{ # try
+
+    ssh deploy@164.90.168.125 "sudo docker system prune -f"
+    #save your output
+
+} || { # catch
+    echo "Done"
+}
